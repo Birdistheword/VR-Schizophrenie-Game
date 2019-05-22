6be14bfa7441b9f62fc15dd5ea4df9db345e5e45
Riccardo <riccardo.fuda@gmx.de>
2019-05-22 11:39:33 +0200

    Neue Szenen und Component
    Testszene für Stimmenspieler erstellt
    Neues Component: OpenUiOnClick -> öffnet Buttonelemente wenn man auf ein Object klickt

---
+++ Assets/Scripts/ClickOpenUi.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickOpenUi : MonoBehaviour
+{
+    [SerializeField] public GameObject myPrefab;
+    private Vector3 posUp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        posUp = transform.position;
+        posUp.y += 2.0f;
+      
+        
+        
+    }
+
+    // Update is called once per frame
+    void OnMouseDown()
+    {
+        // Instantiate at position (0, 0, 0) and zero rotation.
+        Instantiate(myPrefab, posUp, Quaternion.identity);
+    }
+}
