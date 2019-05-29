128e20e63dd89f5dca4f2716844f83c76c37243a
Keja0809 <kerstinlimberger@gmx.at>
2019-05-29 21:56:39 +0200

    Aktuelle Version Kerstin
    Beleuchtung und Farbe des Hauses

---
+++ Assets/Scripts/changeSize.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class changeSize : MonoBehaviour
+
+{
+    public GameObject cube;
+
+
+    private bool scaleUpBool = false;
+    private bool scaleDownBool = false;
+
+    void Start()
+    {
+         scaleUpBool = false;
+         scaleDownBool = false;
+    }
+
+
+    public void scaleUp()
+    {
+
+        scaleUpBool = true;     
+        print("ScaleUp:" + scaleUpBool);
+    }
+
+    public void scaleDown()
+    {
+
+        scaleDownBool = true;
+    }
+
+    void Update(int x)
+    {
+        print("Update:" + scaleUpBool);
+        if (x == 1 ) {
+            cube.transform.localScale += new Vector3(1F, 1F, 1F) * Time.deltaTime;
+        }
+        else if (x == -1)
+        {
+            cube.transform.localScale += new Vector3(-1F, -1F, -1F) * Time.deltaTime;
+        }
+    }
+      
+}
