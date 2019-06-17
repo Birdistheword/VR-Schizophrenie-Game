a94bd6125adce98ebce2894d7d9310fc3456fe37
Keja0809 <kerstinlimberger@gmx.at>
2019-06-17 20:01:02 +0200

    OpenDoor
    Türen öffnen sich über Trigger, funktioniert aber nur bei der unteren Tür. Die anderen Türen öffnen sich zwar auch, aber man kann nicht in den Raum weil der Player von irgendwas blockiert wird.

--- Assets/Scripts/doorOpen.cs
+++ Assets/Scripts/doorOpen.cs
@@ -2,7 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class doorOpen : MonoBehaviour
 {
- 
+    private Animator _animator;
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+             
     }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Untagged")
+        {
+            _animator.SetBool("open", true);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
