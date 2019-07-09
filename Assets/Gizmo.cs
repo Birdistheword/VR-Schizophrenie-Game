39b2f5b8924976ec9631f96741ebcc0fe69db0ac
Keja0809 <kerstinlimberger@gmx.at>
2019-07-09 13:29:56 +0200

    Uhr Issue
    Uhr bewegt sich (Zeiger)
    Lampen Animation in Küche gefixt

---
+++ Assets/Gizmo.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Gizmo : MonoBehaviour
+{
+    public float gizmoSize = .75f;
+    public Color gizmoColor = Color.yellow;
+
+    // Start is called before the first frame update
+    void onDrawGizmos()
+    {
+        Gizmos.color = gizmoColor;
+        Gizmos.DrawWireSphere(transform.position, gizmoSize);
+    }
+
+
+}
