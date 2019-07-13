981b940b4132ebe732075bfbc574027c6cb02ca1
Riccardo <riccardo.fuda@gmx.de>
2019-07-13 12:03:26 +0200

    SinglePlayer Update
    Multiplayer ist raus.
    Player wieder direkt in der Main Szene
    SendEvent und ReceiveEventPosition eingebaut

---
+++ Assets/Scripts/ObjectBehaviour/ReceiveEventPosition.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReceiveEventPosition : MonoBehaviour
+{
+    public GameObject thisObject;
+    public Vector3 PositionBeiEnter;
+    public Vector3 PositionBeiExit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    void messageReceiver(bool trigger)
+    {
+
+        if (trigger == true) thisObject.transform.position = PositionBeiEnter;
+        else if (trigger == false) thisObject.transform.position = PositionBeiExit;
+
+    }
+
+}
---
+++ Assets/Scripts/ObjectBehaviour/SendEvent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendEvent : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject targetObject;
+    private bool trigger;
+
+
+    void Start()
+    {
+        
+    }
+    private void OnTriggerEnter (Collider col)
+    {
+        trigger = true;
+        targetObject.SendMessage("messageReceiver", trigger);
+        Debug.Log("EventSentEnter");
+        
+    }
+    private void OnTriggerExit(Collider col)
+    {
+        trigger = false;
+        targetObject.SendMessage("messageReceiver", trigger);
+        Debug.Log("EventSentLeave");
+    }
+
+}
