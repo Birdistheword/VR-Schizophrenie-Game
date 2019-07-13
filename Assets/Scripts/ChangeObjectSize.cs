5c80a4af46543d828d4fa262d21bccd05d753102
Keja0809 <kerstinlimberger@gmx.at>
2019-07-13 12:19:22 +0200

    Video Bildrauschen

---
+++ Assets/Scripts/ChangeObjectSize.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeObjectSize : MonoBehaviour
+{
+    Vector3 minScale;
+    public Vector3 maxScale;
+    public bool repeatable;
+    public float speed = 0.1f;
+    public float duration = 5f;
+
+IEnumerator Start()
+    {
+        minScale = transform.localScale;
+        while (repeatable)
+        
+        {
+            yield return RepeatLerp(minScale, maxScale, duration);
+            yield return RepeatLerp(maxScale, minScale, duration);
+
+
+        }
+    }
+    public IEnumerator RepeatLerp (Vector3 a, Vector3 b, float time)
+    {
+        float i = 0.0f;
+        float rate = (1.0f / time) * speed;
+        while (i < 1.0f)
+        {
+            i += Time.deltaTime * rate;
+            transform.localScale = Vector3.Lerp(a, b, i);
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/NoteScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Image noteImage;
+
+    public AudioClip pickupSound;
+    public AudioClip putAwaySound;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        noteImage.enabled = false;
+    }
+
+    public void ShowNoteImage()
+    {
+        noteImage.enabled = true;
+        GetComponent<AudioSource>().PlayOneShot(pickupSound);
+    }
+
+    public void HideNoteImage()
+    {
+        noteImage.enabled = false;
+        GetComponent<AudioSource>().PlayOneShot(putAwaySound);
+    }
+}
+
