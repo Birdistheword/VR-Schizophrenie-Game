66bcb1ff72713fbc56df0ca791aaaa603a830db1
Riccardo <riccardo.fuda@gmx.de>
2019-07-16 17:02:21 +0200

    Component GlueTOgether WhenTouched
    Man sucht das Objekt aus bei dem das geworfene Objekt kleben bleiben soll. Bei Collision wird der Rigidbody entfernt und das Objekt klebt für immer und ewig an der Stelle fest.

---
+++ Assets/Scripts/ObjectBehaviour/GlueTogetherWhenTouched.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlueTogetherWhenTouched : MonoBehaviour
+{
+    public GameObject target;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        ContactPoint contact = col.contacts[0];
+        Vector3 pos = contact.point;
+        if (col.gameObject == target) Destroy(GetComponent<Rigidbody>());
+    }
+
+}
