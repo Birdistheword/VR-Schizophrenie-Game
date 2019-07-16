2fb73838f375c3668c364261a38941c71e7a517a
Riccardo <riccardo.fuda@gmx.de>
2019-07-16 15:21:12 +0200

    GrowWhenApproached hinzugefügt
    3 Parameter:
    Player -> player einfügen
    DistanceToStartGrowing -> maximale Distanz ab der Script starten soll
    maxScale: Maximale Größe des Objekts (1.3-2.5)

---
+++ Assets/Scripts/ObjectBehaviour/GrowWhenApproached.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowWhenApproached : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 maxScale;
+    private Vector3 minScale;
+    private Vector3 grow;
+    
+    public int DistanceToStartGrowing;
+
+
+
+    void Start()
+    {
+        minScale = transform.localScale;
+    }
+
+    void Update()
+    {
+        float distance = Vector3.Distance(gameObject.transform.position, player.position);
+
+        if (distance <= DistanceToStartGrowing && distance != 0)
+        {
+            float scale = (1 / distance) * 5;
+            grow = new Vector3(scale, scale, scale);
+            if (grow.x <= maxScale.x) transform.localScale = grow;
+        }
+
+    }
+
+}
