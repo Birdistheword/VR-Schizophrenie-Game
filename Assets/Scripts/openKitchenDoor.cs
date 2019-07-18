9f502796fffc646e7e26f2569954dbcd57350297
Keja0809 <kerstinlimberger@gmx.at>
2019-07-18 16:01:13 +0200

    Dekoration, Küchenschrank und Kühlschrank onKey open
    Blümchen, Teller etc. eingefügt
    
    Küchenschrank und Kühlschrank lassen sich mit 1,2,3,4 öffnen und schließen
    --> on Trigger einbauen

---
+++ Assets/Scripts/openKitchenDoor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openKitchenDoor : MonoBehaviour
+{
+
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    void OnTriggerEnter(Collider other)
+    {
+    
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            anim.Play("openKitchendoor");
+        }
+        if (Input.GetKeyDown("2"))
+        {
+            anim.Play("closeKitchenDoor");
+        }
+    }
+}
---
+++ Assets/openFridge.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openFridge : MonoBehaviour
+{
+
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        other.CompareTag("Player");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("3"))
+        {
+            anim.Play("openFridge");
+        }
+        if (Input.GetKeyDown("4"))
+        {
+            anim.Play("closeFridge");
+        }
+    }
+}
