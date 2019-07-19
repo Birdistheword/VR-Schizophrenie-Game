b693482f708e8174c26031c9750c97ac6f7b6ca9
Keja0809 <kerstinlimberger@gmx.at>
2019-07-19 14:25:27 +0200

    ChangeSzene
    Mit Trigger auf der Terasse

---
+++ Assets/ChangeSzene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSzene : MonoBehaviour
+{
+ public string loadLevel;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(loadLevel);
+        }
+    }
+
+
+
+
+
+}
