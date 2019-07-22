553716d38451fe37b4aceb0510e234bd10746bc0
Riccardo <riccardo.fuda@gmx.de>
2019-07-22 16:17:27 +0200

    Revert "Revert "Component: PlayAnimationIsStateIsTrue""
    This reverts commit 3e6c7f6170bdb074235c0e5ca0baea34f202a7f3.

--- Assets/Scripts/ObjectBehaviour/InteractionTrigger.cs
+++ Assets/Scripts/ObjectBehaviour/InteractionTrigger.cs
@@ -8,6 +8,7 @@ public class InteractionTrigger : MonoBehaviour
     public GameObject[] objectToCheck;
     public string[] stateToSet;
     public AudioClip[] audioClip;
+    Animator animator;
     GameObject player;
     GameObject StateHandler;
     float distanceToTarget = 100f;
@@ -38,12 +39,12 @@ public class InteractionTrigger : MonoBehaviour
             {
                 // State 0 success
                 InteractionTriggerObject();
-                Debug.Log("State Nr. " + counter + " is true");
 
                 if (success == true)
                 {
                     //Debug.Log("BoolToCheck:" + stateToCheck[counter] + " and value:" + StateHandler.GetComponent<StateHandler>().AllBools[stateToCheck[counter]]);
                     //Debug.Log("BoolToSet:" + stateToSet[counter] + " and value:" + StateHandler.GetComponent<StateHandler>().AllBools[stateToSet[counter]]);
+                   
                     StateHandler.GetComponent<StateHandler>().AllBools[stateToSet[counter]] = true;
                     if (audioClip.Length > counter) GameObject.Find("AudioHandler").SendMessage("PlaySound", audioClip[counter]);
                     counter++;
