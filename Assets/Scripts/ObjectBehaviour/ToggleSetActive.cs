3aecf0dcfca2cc77fbde4d154f1b18145060cac7
Riccardo <riccardo.fuda@gmx.de>
2019-07-23 12:30:18 +0200

    ToggleSetActive
    Neues GameObject in Szene erstellen: StateToggler
    Das zu aktivierende Objekt im Inspector aktiviert lassen!
    Dann Component TOggleSetActive geben und dort Objekt + state definieren.
    Bei mehreren Objekten das Component mehrmals geben

---
+++ Assets/Scripts/ObjectBehaviour/ToggleSetActive.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleSetActive : MonoBehaviour
+{
+
+    [SerializeField] GameObject objectToSet;
+    [SerializeField] string stateToCheck;
+    GameObject stateHandler;
+    bool toggle = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        stateHandler = GameObject.Find("StateHandler");
+        Debug.Log("Setting Game Object to inactive");
+        objectToSet.SetActive(false);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (toggle == false)
+        {
+            if (stateHandler.GetComponent<StateHandler>().AllBools[stateToCheck].Equals(true))
+            {
+                objectToSet.SetActive(true);
+               
+                Debug.Log("Setting Game Object to active");
+                toggle = true;
+            }
+        }
+    }
+}
