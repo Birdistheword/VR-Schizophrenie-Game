59274bcd788bfeb7a521181249939a0bc3870bef
Riccardo <riccardo.fuda@gmx.de>
2019-07-25 21:07:18 +0200

    Traumwelt AudioHandler fix

---
+++ Assets/Scripts/Audio/AudioHandlerTraumwelt.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioHandlerTraumwelt : MonoBehaviour
+{
+    // Es muss eine ANzahl an AudioClips rausgesucht werden, die zum Random abspielen geeignet ist.
+
+    AudioSource Audio;
+    GameObject targetObject;
+    int randomNumber = 0;
+
+    void Start()
+    {
+        Audio = GetComponent<AudioSource>();
+
+    
+
+    }
+
+    void Update()
+    {
+
+    }
+
+
+    // Die Funktion kann aufgerufen werden von: SendEvent.cs und InteractionTrigger.cs
+    // Beide haben vordefinierte Sounddateien (Spezifisch für die Aktion)
+    private void PlaySound(AudioClip audioClip)
+    {
+        if (!Audio.isPlaying) Audio.PlayOneShot(audioClip);
+    }
+
+}
